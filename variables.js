633ec388f57088529b3f3597b73b3f031d76fbdf
Peder Goodman <peder.goodman@gmail.com>
2023-05-08 19:35:00 -0500

    Add index and js file

---
+++ variables.js
@@ -0,0 +1,12 @@
+console.log('js');
+
+let myVar = 9;
+
+console.log(myVar);
+
+console.log('The value of myVar is', myVar);
+
+
+let myString = "The variable of myVar has a value of "
+
+console.log(myString + myVar);
